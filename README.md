a7d0dabdd3b16b2b0567e857c4e9951f20733616
alok kummar <alokyadav6635@gmail.com>
2023-06-06 09:24:38 +0530

    add all file

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# albumlist-react
+# React-Albumlist
+React Album list is a responsive gallery component
+React Album supports features - add new album, update and delete.
+
+##
+I have added components - addalbum, albumslist, list, app, Navbar, updatealbum files in components folder. Index.js, Index.css files in src folder. 
+
+
---
+++ src/components/AlbumsList.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import List from './List'
+import Navbar from './Navbar'
+
+
+const AlbumsList= (props) => {
+  return (
+    <>
+      {/* Navbar */}
+      <Navbar page="Add Album" path="/add-album" />
+
+      <div className='Album-Collection'>
+        {props.albums.map((album) => <List album={album} key={album.id} setUpdateAlbum={props.setUpdateAlbum} deleteAlbumFromList={props.deleteAlbumFromList} />)}
+      </div>
+    </>
+  )
+}
+
+export default AlbumsList
